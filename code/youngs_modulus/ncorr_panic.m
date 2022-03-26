5bb132537eb82f9e770ece1f8217c0cd0e1eb789
Mingde Yin <mingdeyin@gmail.com>
2022-03-26 16:25:15 -0400

    ncorr panik

--- code/youngs_modulus/ncorr_panic.m
+++ code/youngs_modulus/ncorr_panic.m
@@ -1,14 +1,18 @@
 load("../../data/processed_labview/specimens.mat", "specimens");
 
-i = 1;
+
+E = zeros(1, 5);
 
 indices = [101, 144, 144, 30, 302];
 times = [105, 150, 150, 30, 315];
-offsets = [0 0 0 0 0];
+offsets = [-30 0 0 0 0];
 images = times / 15;
 
-axial_strains = []
+axial_strains = [0.0012, 0.0007, 0.0003, 0.0002, 0.025];
 
-
-stress = specimens{i}.stress(indices(i))
-
+for i = 1:5
+    stress = specimens{i}.stress(indices(i)+offsets(i))
+    reference = specimens{i}.strain_axial(indices(i))
+    E(i) = stress / axial_strains(i) / 1000
+    E_ref = stress / reference / 1000;
+end
